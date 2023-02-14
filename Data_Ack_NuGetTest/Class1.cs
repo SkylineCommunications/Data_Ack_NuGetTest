1388689a5c493946b1cdeadfbf5b470fe5816a9e
Jan Staelens <jan.staelens@skyline.be>
2023-02-14 14:21:04 +0100

    Should definitely not pass validation step.

---
+++ Data_Ack_NuGetTest/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Data_Ack_NuGetTest
+{
+    public class DataAckTest
+    {
+        /// <summary>
+        /// A test method.
+        /// </summary>
+        /// <returns>Returns <see cref="bool"/> as true.</returns>
+        public bool JustATest()
+        {
+            return true;
+        }
+    }
+}
